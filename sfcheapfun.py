4ce203c898ec19e194d4ad4c08a868ab10428f02
Jason Goldman <jason@upparent.com>
2016-12-27 16:41:25 -0600

    next

--- sfcheapfun.py
+++ sfcheapfun.py
@@ -38,7 +38,7 @@ for e in elements:
 			events[current_el] = []
 		else:
 			event_detail = e.find(attrs={'class': 'title'}).find('a')
-			events[current_el].append('\t<'+event_detail['href'] + '|' + event_detail.text+'>')
+			events[current_el].append(' \t<'+event_detail['href'] + '|' + event_detail.text+'>')
 	except:
 		pass
 months = {'01': 'january','02': 'february','03': 'march','04': 'april','05': 'may','06': 'june','07': 'july','08': 'august','09': 'september','10': 'october','11': 'november','12': 'december'}
