eebff40e67269f96ca7d6ef9298099304beea583
Jason Goldman <jason@upparent.com>
2016-12-30 09:22:20 -0600

    test

--- eventGuide.py
+++ eventGuide.py
@@ -6,12 +6,14 @@ import requests
 wdir = './scrapers/'
 for scraper in os.listdir(wdir):
 	new_events = subprocess.call(["python", wdir + scraper])
+	print(new_events)
 	for key, value in new_events.items():
 		domain = key.split('/')[0]
-		sc.api_call(
-		  "chat.postMessage",
-		  # channel="#sf_events",
-		  channel="#general",
-		  username="eventGuide",
-		  text='<' + key +'| Top Events of ' + domain + ' for ' + time.strftime('%m/%d') + ':>\n>>>' + '\n'.join(value)
-		)
+		print(key)
+		# sc.api_call(
+		#   "chat.postMessage",
+		#   # channel="#sf_events",
+		#   channel="#general",
+		#   username="eventGuide",
+		#   text='<' + key +'| Top Events of ' + domain + ' for ' + time.strftime('%m/%d') + ':>\n>>>' + '\n'.join(value)
+		# )
