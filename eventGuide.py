01abf56c0784d5a9b614f49cd5ed64cc606cc2a2
Jason Goldman <jason@upparent.com>
2016-12-30 11:25:43 -0600

    auth change

--- eventGuide.py
+++ eventGuide.py
@@ -11,9 +11,7 @@ def main():
     """ 
 	# from scrapers import *
 
-	slack_token = 'xoxp-6354740753-101554727523-122050987026-5d4b0406dbb043415362834569c96c81'
-	# slack_token = 'xoxp-120041460657-120720635442-120652984483-ebf82c75b08c59633355a55bb9b03747'
-	sc = SlackClient(slack_token)
+	sc = SlackClient(sys.argv[1])
 
 	wdir = './scrapers/'
 	for scraper in os.listdir(wdir):
@@ -21,12 +19,12 @@ def main():
 		event_string = json.loads(event_json)
 		for key, value in event_string.items():
 			domain = key.split('/')[2]
-			sc.api_call(
-			  "chat.postMessage",
-			  # channel="#sf_events",
-			  channel="#sf_events",
-			  username="eventGuide",
-			  text='<' + key +'| Top Events of ' + domain + ' for ' + time.strftime('%m/%d') + ':>\n>>>' + '\n'.join(value)
-			)
+			# sc.api_call(
+			#   "chat.postMessage",
+			#   # channel="#sf_events",
+			#   channel="#sf_events",
+			#   username="eventGuide",
+			#   text='<' + key +'| Top Events of ' + domain + ' for ' + time.strftime('%m/%d') + ':>\n>>>' + '\n'.join(value)
+			# )
 if __name__ == "__main__":
   main() 
