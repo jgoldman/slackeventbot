6339540c468298beb5b00fa5ca340517d51fad8f
Jason Goldman <jason@upparent.com>
2016-12-30 10:54:40 -0600

    fixed project structure

--- eventGuide.py
+++ eventGuide.py
@@ -1,32 +1,32 @@
 import os
 import subprocess 
 import time
-# from slackclient import SlackClient
+from slackclient import SlackClient
 import sys
-from scrapers import *
+import json
 
-# slack_token = 'xoxp-6354740753-101554727523-121009606037-535cfd995d88810d73cf1a3283ae9a84'
-slack_token = 'xoxp-120041460657-120720635442-120652984483-ebf82c75b08c59633355a55bb9b03747'
-# sc = SlackClient(slack_token)
+def main():
+	"""
+    Per scraper in ./scrapers send a message to slack
+    """ 
+	# from scrapers import *
 
-wdir = './scrapers/'
+	slack_token = 'xoxp-6354740753-101554727523-121009606037-535cfd995d88810d73cf1a3283ae9a84'
+	# slack_token = 'xoxp-120041460657-120720635442-120652984483-ebf82c75b08c59633355a55bb9b03747'
+	sc = SlackClient(slack_token)
 
-test = sffun_scraper
-print(test)
-for scraper in os.listdir(wdir):
-	print()
-	# new_events = subprocess.check_output(['python', wdir + scraper])
-	# print(new_events)
-	
-
-	# for key, value in new_events.items():
-	# 	domain = key.split('/')[0]
-	# 	print('key')
-	# 	print(key)
-	# 	# sc.api_call(
-	# 	#   "chat.postMessage",
-	# 	#   # channel="#sf_events",
-	# 	#   channel="#general",
-	# 	#   username="eventGuide",
-	# 	#   text='<' + key +'| Top Events of ' + domain + ' for ' + time.strftime('%m/%d') + ':>\n>>>' + '\n'.join(value)
-	# 	# )
+	wdir = './scrapers/'
+	for scraper in os.listdir(wdir):
+		event_json = subprocess.check_output(['python', wdir + scraper])
+		event_string = json.loads(event_json)
+		for key, value in event_string.items():
+			domain = key.split('/')[2]
+			sc.api_call(
+			  "chat.postMessage",
+			  # channel="#sf_events",
+			  channel="#sf_events",
+			  username="eventGuide",
+			  text='<' + key +'| Top Events of ' + domain + ' for ' + time.strftime('%m/%d') + ':>\n>>>' + '\n'.join(value)
+			)
+if __name__ == "__main__":
+  main()
--- scrapers/sfcheapfun.py
+++ scrapers/sfcheapfun.py
@@ -1,12 +1,10 @@
-import requests
 from bs4 import BeautifulSoup
-from slackclient import SlackClient
 import time
-slack_token = 'xoxp-6354740753-101554727523-121009606037-535cfd995d88810d73cf1a3283ae9a84'
-# slack_token = 'xoxp-120041460657-120720635442-120652984483-ebf82c75b08c59633355a55bb9b03747'
-sc = SlackClient(slack_token)
+import sys
+import json
+import requests
 
-def grab_html_source(link):
+def get_html(link):
    """
    Grab the html source for a link, return it as a string.
    """        
@@ -19,41 +17,36 @@ def grab_html_source(link):
        return r.content
    else:
        print("ERROR: " + link)
-       return grab_html_source(link)
+       return get_html(link)
 
-html = grab_html_source('http://sf.funcheap.com/category/event/top-pick/')
-soupHTML = BeautifulSoup(html, 'lxml')
-# days = soupHTML.find_all(attrs={'class': 'archive_date_title'})
+def main():
+  """
+   Grab the html source for a link, return it as a string.
+  """        
 
-elements = soupHTML.find(attrs={'id': 'content'}).find(attrs={'class': 'clearfloat'}).find_all('div')
+  html = get_html('http://sf.funcheap.com/category/event/top-pick/')
+  soupHTML = BeautifulSoup(html, 'lxml')
+  elements = soupHTML.find(attrs={'id': 'content'}).find(attrs={'class': 'clearfloat'}).find_all('div')
 
-current_el = None
-events = {}
+  current_el = None
+  events = {}
 
-for e in elements:
-	try:
-		class_string = ''.join(e['class'])
-		if class_string == 'archive_date_title':
-			current_el = e.text.split(', ')[1].lower().split(' ')[1]
-			events[current_el] = []
-		else:
-			event_detail = e.find(attrs={'class': 'title'}).find('a')
-			events[current_el].append('<'+event_detail['href'] + '|' + event_detail.text+'>')
-	except:
-		pass
-months = {'01': 'january','02': 'february','03': 'march','04': 'april','05': 'may','06': 'june','07': 'july','08': 'august','09': 'september','10': 'october','11': 'november','12': 'december'}
-# day_list = [d.text.split(', ')[1].lower().split(' ') for d in days]
+  months = {'01': 'january','02': 'february','03': 'march','04': 'april','05': 'may','06': 'june','07': 'july','08': 'august','09': 'september','10': 'october','11': 'november','12': 'december'}
+  current_day =  time.strftime('%d')
 
-current_day =  time.strftime('%d')
-# current_day = current_day.split('/')
+  for e in elements:
+    try:
+      class_string = ''.join(e['class'])
+      if class_string == 'archive_date_title':
+        current_el = e.text.split(', ')[1].lower().split(' ')[1]
+        events[current_el] = []
+      else:
+        event_detail = e.find(attrs={'class': 'title'}).find('a')
+        text = '<'+event_detail['href'] + '|' + event_detail.text+'>'
+        events[current_el].append(text.encode('utf-8'))
+    except:
+      pass
+  print(json.dumps({'http://sf.funcheap.com/category/event/top-pick/': events[current_day]}))
 
-
-sc.api_call(
-  "chat.postMessage",
-  channel="#sf_events",
-  username="eventGuide",
-  text='<http://sf.funcheap.com/category/event/top-pick/|Top Events for ' + time.strftime('%m/%d') + ':>\n>>>' + '\n'.join(events[current_day])
-)
-		
-	
-
+if __name__ == "__main__":
+  main()
